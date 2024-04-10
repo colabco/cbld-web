1e5daf3875f8492d1a78dbbe214ab018977f3e05
Elizabeth Chapman <elizabethmaychapman@gmail.com>
2024-04-10 18:03:34 +0100

    chore: edit name and description

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar: React.FC = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Co:Build Collective</a>
+        <a className="btn btn-ghost text-xl">Co:Build</a>
       </div>
       <div className="flex-none">
         <button className="btn btn-square btn-ghost">
